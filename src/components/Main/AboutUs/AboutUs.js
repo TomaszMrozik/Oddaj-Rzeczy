7f7f6c418115352f1ccd646fa2edc7325fe186be
Tomasz Mrozik <tomaszmrozik2000@gmail.com>
2021-08-10 18:36:50 +0200

    about us -content

--- src/components/Main/AboutUs/AboutUs.js
+++ src/components/Main/AboutUs/AboutUs.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import PeopleImg from '../../../assets/People.jpg';
+import AccentImg from "../../Decorations/AccentImg";
+import Signature from "../../../assets/Signature.svg";
 
 const AboutUs = () => {
     return (
         <div id={'aboutUs'}>
-
+            <div className={'aboutUsInformation'}>
+                <h3>O nas</h3>
+                <AccentImg/>
+                <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <img src={Signature} alt={'signature'} className={'signature'}/>
+            </div>
+            <img src={PeopleImg} alt={'About_Us'} className={'peopleImg'}/>
         </div>
     );
 };
